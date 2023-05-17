ee881d5b44bf54efcd3db49946be663cac91c30b
Ashraf59 <ashrafams59b@gmail.com>
2023-05-18 02:29:55 +0600

    Responsive shopping card completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,90 @@ const prevBtn = document.querySelector("[data-prev]");
 
 
 
+//Set the slider default position
+
+let sliderPos = 0;
+
+//Set the total slider items
+
+let totalSliderItem = 4;
+
+//make next slide btn workable
+
+const slideToNext = function () {
+    sliderPos++;
+    slider.style.transform = `translateX(-${sliderPos}00%)`
+    sliderEnd();
+}
+
+addEventOnElem(nextBtn, "click", slideToNext);
+
+//make prev slide btn workable
+
+const slideToPrev = function () {
+    sliderPos--;
+    slider.style.transform = `translateX(-${sliderPos}00%)`
+    sliderEnd();
+}
+
+addEventOnElem(prevBtn, "click", slideToPrev);
+
+
+// check when slider is end then what should slider btn do
+
+function sliderEnd (){
+    if(sliderPos >= totalSliderItem - 1){
+        nextBtn.classList.add("disabled");
+    } else {
+        nextBtn.classList.remove("disabled");
+    }
+
+    if(sliderPos <= 0){
+        prevBtn.classList.add("disalbed");
+    } else {
+        prevBtn.classList.remove("disabled");
+    }
+
+}
+
+sliderEnd();
+
+
+
 /**
- * Slider Funtionality
+ * product quantity functionality
  */
+    const totalPriceElem  = document.querySelector("[data-total-price]");
+    const qtyElem = document.querySelector("[data-qty]");
+    const qtyMinusBtn = document.querySelector("[data-qty-minus]");
+    const qtyPlusBtn = document.querySelector("[data-qty-plus]");
+
+    // set the product default quantity
+    let qty = 1;
+
+    // set the product default price
+    let productPrice = 125;
+
+    // set the initial total price
+    let totalPrice = 125;
+
+
+    const increaseProductQty = function (){
+        qty++;
+        totalPrice = qty * productPrice;
+
+        qtyElem.textContent = qty;
+        totalPriceElem.textContent = `$${totalPrice}.00`;
+    }
+
+    addEventOnElem(qtyPlusBtn, "click", increaseProductQty);
+
+    const decreaseProductQty = function () {
+        if(qty > 1) qty--;
+        totalPrice = qty * productPrice;
+
+        qtyElem.textContent = qty;
+        totalPriceElem.textContent = `$${totalPrice}.00`;
+    }
+
+    addEventOnElem(qtyMinusBtn, "click", decreaseProductQty)
